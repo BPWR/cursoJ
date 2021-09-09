ea4feda9d16975168e3368fb842e3bf21b9b35ff
BPWR <benruicor@hotmail.es>
2021-09-09 15:57:04 +0200

    actualizo figuras

--- figuras.js
+++ figuras.js
@@ -1,1 +1,63 @@
-console.log("hello world")
+// Código del cuadrado
+console.group("Cuadrados")
+const ladoCuadrado = 7;
+console.log("los lados del cuadrado miden: " + ladoCuadrado + " cm")
+
+const perimetroCuadrado = ladoCuadrado * 4; 
+console.log("el perimetro del cuadrado es: " + perimetroCuadrado + " cm")
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado; 
+console.log("el área del cuadrado es: " + areaCuadrado + " cm2")
+console.groupEnd
+
+// Código del triángulo
+console.group("Triángulos")
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+
+console.log(
+    "los lados del triángulo miden: " +
+    + ladoTriangulo1
+    + " cm, "
+    + ladoTriangulo2
+    + " cm, "
+    + baseTriangulo
+    + " cm"
+)
+
+console.log("la altura del triángulo es: " + alturaTriangulo + " cm")
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo; 
+console.log("el perimetro del triángulo es: " + perimetroTriangulo  + " cm")
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2; 
+console.log("el área del triángulo es: " + areaTriangulo + " cm2")
+
+console.groupEnd
+
+// Código del círculo
+console.group("Cirulo")
+
+const 
+
+console.log(
+    "los lados del triángulo miden: " +
+    + ladoTriangulo1
+    + " cm, "
+    + ladoTriangulo2
+    + " cm, "
+    + baseTriangulo
+    + " cm"
+)
+
+console.log("la altura del triángulo es: " + alturaTriangulo + " cm")
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo; 
+console.log("el perimetro del triángulo es: " + perimetroTriangulo  + " cm")
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2; 
+console.log("el área del triángulo es: " + areaTriangulo + " cm2")
+
+console.groupEnd
